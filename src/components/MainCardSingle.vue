74dd3751bcb8a63831ae24126dde15299ce10feb
Joel Mitchell <joey5533a@gmail.com>
2025-10-04 13:38:15 -0500

    Trying to get the pics to show

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -5,12 +5,12 @@ const firstname = faker.person. firstName()
 const lastname = faker.person. lastName ()
 const job_title = faker.person. jobTitle()
 const bio = faker.lorem.lines(2)
-const username = faker. internet. userName({firstName: firstname, lastName: lastname}).toLowerCase()
+const username = faker.internet.userName({firstName: firstname, lastName: lastname}).toLowerCase()
 
 </script>
 
 <template>
-    <!-- {{ fullname }} <br /> -->
+    <!-- ={{ fullname }} <br /> -->
     <div>
         <img :src="faker.image.urlLoremFlickr({category: 'dog'})" />
     </div>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,7 +1,7 @@
 <script setup>
-//import { ref } from 'vue'
+import { ref } from 'vue'
 //const title = ref('Hello There ✋')
-import MainCards from '@/components/MainCards.vue';
+import MainCards from '@/components/MainCards.vue'
 </script>
 
 <template>
