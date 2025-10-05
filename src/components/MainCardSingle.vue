c347bab783304f6bcf1988b9544086c6ea147482
Joel Mitchell <joey5533a@gmail.com>
2025-10-05 12:50:19 -0500

    card data and style done

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -10,8 +10,13 @@ const username = faker.internet.username({firstName: firstname, lastName: lastna
 </script>
 
 <template>
-
-    <div>
-        <img class="object-fill h-480 w-960" v-bind:src="faker.image.urlLoremFlickr({category: 'dog', height: 480, width: 960})" />
+    <!-- {{ fullname }} <br /> -->
+    <div class="border border-gray-400 rounded-lg shadow bg-gray-200-h-full">
+        <img class="object-fill h-480 w-960" v-bind:src="faker.image.urlLoremFlickr({category: 'cat', height: 480, width: 960})" />
+        <div class=":p-4">
+            <p class="mb-2 text 2x1 font-bold">{{ fullname }}</p>
+            <p class="mb-3 font-normal text-gray-700">{{ job_title }}</p>
+            <p class="mb-3 font-normal text-gray-700">{{ bio }}</p>
+        </div>
     </div>
 </template>
