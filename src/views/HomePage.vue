2ae650611acbd3cd1d855b7fe264cec4bade5c26
Joel Mitchell <joey5533a@gmail.com>
2025-10-05 17:33:35 -0500

    pagination working

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,9 +1,11 @@
 <script setup>
 //import { ref } from 'vue'
 //const title = ref('Hello There ✋')
-import MainCards from '@/components/MainCards.vue'
+import MainCards from '@/components/MainCards.vue';
+import MainPagination from '@/components/MainPagination.vue';
 </script>
 
 <template>
   <MainCards></MainCards>
+  <MainPagination></MainPagination>
 </template>
