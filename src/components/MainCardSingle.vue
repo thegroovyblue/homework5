e8dd561df3c4938c07b54d72bbfc248cbb8d301c
Joel Mitchell <joey5533a@gmail.com>
2025-10-05 12:59:28 -0500

    fixed rounded corners on image

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -12,7 +12,7 @@ const username = faker.internet.username({firstName: firstname, lastName: lastna
 <template>
     <!-- {{ fullname }} <br /> -->
     <div class="border border-gray-400 rounded-lg shadow bg-gray-200-h-full">
-        <img class="object-fill h-480 w-960" v-bind:src="faker.image.urlLoremFlickr({category: 'cat', height: 480, width: 960})" />
+        <img :src="faker.image.urlLoremFlickr({ category: 'cat' })" class="rounded-t-x" />
         <div class=":p-4">
             <p class="mb-2 text 2x1 font-bold">{{ fullname }}</p>
             <p class="mb-3 font-normal text-gray-700">{{ job_title }}</p>
