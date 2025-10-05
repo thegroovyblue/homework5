9dcbbf71f5d4c8b43d2e2ff8290be87da93dee6b
Joel Mitchell <joey5533a@gmail.com>
2025-10-05 02:50:45 -0500

    Pic still not showing, saving before continuing.

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -2,21 +2,16 @@
 import { faker } from '@faker-js/faker'
 const fullname = faker.person. fullName()
 const firstname = faker.person. firstName()
-const lastname = faker.person. lastName ()
+const lastname = faker.person. lastName()
 const job_title = faker.person. jobTitle()
 const bio = faker.lorem.lines(2)
-const username = faker.internet.userName({firstName: firstname, lastName: lastname}).toLowerCase()
-faker.image.urlPicsumPhotos({ width: 640, height: 480 })
+const username = faker.internet.username({firstName: firstname, lastName: lastname}).toLowerCase()
+
 </script>
 
 <template>
-    <article class="card">
-    <img :src="imgUrl" alt="random dog" />
-    <h3>{{ fullName }}</h3>
-  </article>
-</template>
 
-<style scoped>
-.card{padding:8px}
-img{width:100%;display:block;border-radius:.5rem}
-</style>
+<div>
+    <img :src="faker.image.urlLoremFlickr({category: 'dog'})"/>
+</div>
+</template>
--- src/components/MainCards.vue
+++ src/components/MainCards.vue
@@ -1,17 +1,10 @@
 <script setup>
-import MainCardSingle from './MainCardSingle.vue'
+import MainCardSingle from './MainCardSingle.vue';
+
 </script>
 
 <template>
-  <div class="grid">
-    <MainCardSingle v-for="n in 12" :key="n" />
-  </div>
-</template>
 
-<style scoped>
-.grid{
-  display:grid;
-  grid-template-columns:repeat(auto-fill,minmax(220px,1fr));
-  gap:16px; padding:16px;
-}
-</style>
+<MainCardSingle v-for="card in 20" :key="card"></MainCardSingle>
+
+</template>
